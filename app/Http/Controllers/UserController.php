8673719a3a58326db48fd6c89229e6dd7f5bb28f
Thomas van Doorn <admin@thomasdomein.nl>
2017-05-07 00:09:28 +0200

    User list finished - User add WIP

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,26 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Models\User;
 use Illuminate\Http\Request;
 
-class UserController extends Controller
-{
-    //
-    public function index() {
-        return view('user.index');
-    }
+class UserController extends Controller {
+    
+    public function showUsers() {
+        $apiresponse = ApiController::doRequest('GET', '/users?perPage=200', ["bearer" => AuthController::getToken()], []);
+        $userdata = \GuzzleHttp\json_decode($apiresponse->getBody());
 
-    public function getusers() {
-        $users = new Collection;
-        $faker = Faker::create();
-
-        for ($i = 0; $i < 100; $i++) {
-            $users->push([
-                'id'         => $i + 1,
-                'name'       => $faker->name,
-                'email'      => $faker->email,
-            ]);
+        $users = [];
+        
+        foreach($userdata->data as $user) {
+            $users[count($users)] = new User($user);
         }
-        return Datatables::of($users)->make(true);
+        
+        return view('user.list', ['users' => $users]);
     }
+    
+    public function showAddUser() {
+        return view('user.add');
+    }
+    public function addUser(Request $request) {
+        $this->validata($request, [
+            'firstname' => 'required|string|min:3',
+            'lastname' => 'required|string|min:3',
+            'email' => 'required|email',
+            'role' => '',
+            'region' => '',
+        ]);
+    }
+    
 }
