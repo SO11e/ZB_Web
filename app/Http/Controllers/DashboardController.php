7c71ea6acbb7cd501f613b5aebbdd0551a33ddfe
Thomas van Doorn <admin@thomasdomein.nl>
2017-05-27 17:59:41 +0200

    Finished dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Auth\AuthController;
+
 class DashboardController extends BaseController {
     
     public function index() {
-        return view('dashboard');
+        $apiresponse = ApiController::doRequest('GET', '/issues?perPage=200', ["bearer" => AuthController::getToken()], []);
+        $issuedata = \GuzzleHttp\json_decode($apiresponse->getBody());
+        
+        $issuecount = 0;
+        foreach($issuedata->data as $issue) {
+            if(isset($issue->status) && $issue->status == 'open')
+                $issuecount++;
+        }
+    
+        $apiresponse2 = ApiController::doRequest('GET', '/reports?perPage=200', ["bearer" => AuthController::getToken()], []);
+        $reportdata = \GuzzleHttp\json_decode($apiresponse2->getBody());
+    
+        $createdreports = 0;
+        $sentreports = 0;
+        foreach($reportdata->data as $report) {
+            if(isset($report->status) && $report->status == 'created')
+                $createdreports++;
+            
+            if(isset($report->status) && $report->status == 'send')
+                $sentreports++;
+        }
+        
+        return view('dashboard', [
+            'issuecount' => $issuecount,
+            'createdreports' => $createdreports,
+            'sentreports' => $sentreports,
+        ]);
     }
 }
